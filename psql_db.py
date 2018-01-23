18ade4754583368baed33602eef0b8acaec233d3
Trevor Roman <troman360@gmail.com>
2018-01-23 00:17:45 -0500

    Fixed docstrings according to pydocstyle linter and added shebang.

--- psql_db.py
+++ psql_db.py
@@ -1,39 +1,42 @@
+#!/usr/bin/env python3
+"""This module simplifies access to a PostgreSQL database."""
 import psycopg2
 from logs_analysis_exceptions import PrintException, PsqlDbException
 from psycopg2.extras import DictCursor
 
 
 class PsqlDb:
-    '''
-        PsqlDb provides convenient wrapper functions for connecting to and
-        executing queries on a PostgreSQL database via psycopg2.
-    '''
+    """PsqlDb provides convenient wrapper functions for psycopg2.
+
+    Upon instantiation, a database name is set. All connections attempted by
+    the instance will attempt to connect to the specified database.
+    """
 
     def __init__(self, database_name):
-        '''
-            Instantiate a PsqlDb with database name. This name will
-            be used for all connections made by this instance.
+        """Instantiate a PsqlDb with database name.
 
-            args:
-                database_name - a string. The name of the database to
-                connect to.
-        '''
+        This name will be used for all connections made by this instance.
+
+        Args:
+            database_name - a string. The name of the database to
+            connect to.
+        """
         self.database_name = database_name
 
     def connect(self):
-        '''
-            Creates a connection to the database. Raises an error if the
-            connection fails for any reason.
+        """Create a connection to the database.
 
-            returns:
-                connection, cursor - a tuple. The database connection
-                and cursor instance. The cusor is formatted to return
-                dictionaries rather than tuples.
+        Raises an error if the connection fails for any reason.
 
-            raises:
-                PsqlDbException - If the connection fails for any reason.
-        '''
+        Returns:
+            connection, cursor - a tuple. The database connection
+            and cursor instance. The cusor is formatted to return
+            dictionaries rather than tuples.
 
+        Raises:
+            PsqlDbException - If the connection fails for any reason.
+
+        """
         print('Attempting to connect to database: {}'
               .format(self.database_name))
 
@@ -51,20 +54,20 @@ class PsqlDb:
             raise PsqlDbException(msg, e)
 
     def query(self, query_string):
-        '''
-            Connects to the database, performs a query, and then closes then
-            connection.
+        """
+        Connect to database, perform query, then closes then connection.
 
-            args:
-                query_string - a string. An SQL query statement.
+        Args:
+            query_string - a string. An SQL query statement.
 
-            returns:
-                results - a list of dictionaries. A list of column name ->
-                index mappings.
+        Returns:
+            results - a list of dictionaries. A list of column name ->
+            index mappings.
 
-            raises:
-                PsqlDbException - If the query fails for any reason.
-        '''
+        Raises:
+            PsqlDbException - If the query fails for any reason.
+
+        """
         connection, cursor = self.connect()
 
         try:
